fe77b8e3839a13e3dc1720250e07f0e6069c4a87
YANGNIGHT <79457406+YANGNIGHT@users.noreply.github.com>
2022-06-05 22:25:32 +0800

    修复ip显示bug

--- auto.py
+++ auto.py
@@ -290,12 +290,11 @@ for x in fhandle:
                 ip = json.loads(r.text)["origin"]  # ip地址，取其中某个字段的值
 
                 # 发送get请求
-                url = f'http://ip-api.com/json/{ip}?fields=status,message,country,countryCode,region,regionName,city,zip,' \
-                      f'lat,lon,timezone,isp,org,as,query&lang=zh-CN '
+                url = f'http://ip-api.com/json/{ip}?fields=status,message,country,countryCode,region,regionName,city,zip,lat,lon,timezone,isp,org,as,query&lang=zh-CN'
                 # 其中fields字段为定义接受返回参数，可不传；lang为设置语言，zh-CN为中文，可以传
-                res = requests.get(url)  # 发送请求
+                res = requests.get(url)		# 发送请求
                 jsonobj = eval(res.text)  # 传回参数转为字典
-                location = '当前服务器ip为:%s,属地为:%s' % (ip, jsonobj['country'] + jsonobj['regionName'] + jsonobj['city'])
+                location = '当前服务器ip为:%s,属地为:%s'% (ip,jsonobj['country']+jsonobj['regionName']+jsonobj['city'])
                 print(location)
 
                 # 邮箱通知
